0a791e105d8534f510a2e567c1a184e253876256
unknown <smatintavakolia@gmail.com>
2021-09-12 22:01:10 +0430

    sensory data received. everything seems fine

--- PythonAPI/examples/agent.py
+++ PythonAPI/examples/agent.py
@@ -1,8 +1,18 @@
+# ==============================================================================
+# -- imports -------------------------------------------------------------------
+# ==============================================================================
+
 import glob
 import os
+import numpy as np
 import sys
 import random
 import time
+import cv2
+
+# ==============================================================================
+# -- import carla --------------------------------------------------------------
+# ==============================================================================
 
 try:
     sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
@@ -12,18 +22,34 @@ try:
 except IndexError:
     pass
 
-# ==============================================================================
-# -- imports -------------------------------------------------------------------
-# ==============================================================================
-
-
 import carla
+
+# ==============================================================================
+# -- constants -----------------------------------------------------------------
+# ==============================================================================
+
+IM_WIDTH = 640
+IM_HEIGHT = 480
+
+
+# ==============================================================================
+# -- functions -----------------------------------------------------------------
+# ==============================================================================
+
+def process_sensory_data(data):
+    data_arr = np.array(data.raw_data)
+    data_pic = data_arr.reshape((IM_HEIGHT, IM_WIDTH, 4))[:, :, :3]  # we only want rgb!
+    cv2.imshow("", data_pic)
+    cv2.waitKey(1)
+    print('reached here')
+    return data_pic / 255.0  # normalizing sensory data for the neural network
+
 
 actor_list = []
 
 try:
     client = carla.Client("localhost", 2000)
-    client.set_timeout(2.0)  # seconds
+    client.set_timeout(4.0)  # seconds
 
     world = client.get_world()  # world connection
     blueprint_library = world.get_blueprint_library()
@@ -31,12 +57,24 @@ try:
     bp = blueprint_library.filter("bmw")[0]  # fetch the first model of bmw
     print("selected blueprint is {}".format(bp))
 
-    spawn_point = random.choice(world.get_map().get_spawn_points())
+    vehicle_spawn_point = random.choice(world.get_map().get_spawn_points())
 
-    vehicle = world.spawn_actor(bp, spawn_point)
+    vehicle = world.spawn_actor(bp, vehicle_spawn_point)
     vehicle.set_autopilot(False)  # making sure its not in autopilot!
     vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=0.0))
     actor_list.append(vehicle)
+
+    # sensory data
+    camera_bp = blueprint_library.find("sensor.camera.rgb")
+    camera_bp.set_attribute("image_size_x", f"{IM_WIDTH}")
+    camera_bp.set_attribute("image_size_y", f"{IM_HEIGHT}")
+    camera_bp.set_attribute("fov", "110")
+
+    camera_spawn_point = carla.Transform(carla.Location(x=5, z=2))  # TODO: fine-tune these values!
+    sensor = world.spawn_actor(camera_bp, camera_spawn_point, attach_to=vehicle)
+    actor_list.append(sensor)
+    sensor.listen(lambda data: process_sensory_data(data))
+
     time.sleep(7)
 
 finally:
