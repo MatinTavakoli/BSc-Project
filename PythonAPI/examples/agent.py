c7959abb6a11445d7d34c3b29fe7c170693a5bad
unknown <smatintavakolia@gmail.com>
2021-09-13 19:40:44 +0430

    migrated code to class CarEnv

--- PythonAPI/examples/agent.py
+++ PythonAPI/examples/agent.py
@@ -11,7 +11,7 @@ import time
 import cv2
 
 # ==============================================================================
-# -- import carla --------------------------------------------------------------
+# -- importing carla -----------------------------------------------------------
 # ==============================================================================
 
 try:
@@ -28,6 +28,7 @@ import carla
 # -- constants -----------------------------------------------------------------
 # ==============================================================================
 
+SHOW_PREVIEW = False
 IM_WIDTH = 640
 IM_HEIGHT = 480
 
@@ -36,48 +37,50 @@ IM_HEIGHT = 480
 # -- functions -----------------------------------------------------------------
 # ==============================================================================
 
-def process_sensory_data(data):
-    data_arr = np.array(data.raw_data)
-    data_pic = data_arr.reshape((IM_HEIGHT, IM_WIDTH, 4))[:, :, :3]  # we only want rgb!
-    cv2.imshow("", data_pic)
-    cv2.waitKey(1)
-    print('reached here')
-    return data_pic / 255.0  # normalizing sensory data for the neural network
+class CarEnv:
+    SHOW_CAM = SHOW_PREVIEW
+    STEER_AMT = 1.0
+    im_width = IM_WIDTH
+    im_height = IM_HEIGHT
+    front_camera = None
 
+    def __init__(self):
+        self.client = carla.Client("localhost", 2000)
+        self.client.set_timeout(4.0)  # seconds
 
-actor_list = []
+        self.world = self.client.get_world()  # world connection
+        self.blueprint_library = self.world.get_blueprint_library()
 
-try:
-    client = carla.Client("localhost", 2000)
-    client.set_timeout(4.0)  # seconds
+        self.model = self.blueprint_library.filter("bmw")[0]  # fetch the first model of bmw
 
-    world = client.get_world()  # world connection
-    blueprint_library = world.get_blueprint_library()
+    def reset(self):
+        self.collision_hist = []
+        self.actor_list = []
 
-    bp = blueprint_library.filter("bmw")[0]  # fetch the first model of bmw
-    print("selected blueprint is {}".format(bp))
+        # spawn vehicle
+        self.vehicle_spawn_point = random.choice(self.world.get_map().get_spawn_points())
+        self.vehicle = self.world.spawn_actor(self.model, self.vehicle_spawn_point)
+        self.vehicle.set_autopilot(False)  # making sure its not in autopilot!
+        self.actor_list.append(self.vehicle)
 
-    vehicle_spawn_point = random.choice(world.get_map().get_spawn_points())
+        # camera sensory data
+        self.camera = self.blueprint_library.find("sensor.camera.rgb")
+        self.camera.set_attribute("image_size_x", f"{IM_WIDTH}")
+        self.camera.set_attribute("image_size_y", f"{IM_HEIGHT}")
+        self.camera.set_attribute("fov", "110")
 
-    vehicle = world.spawn_actor(bp, vehicle_spawn_point)
-    vehicle.set_autopilot(False)  # making sure its not in autopilot!
-    vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=0.0))
-    actor_list.append(vehicle)
+        # spawn camera
+        self.camera_spawn_point = carla.Transform(carla.Location(x=5, z=2))  # TODO: fine-tune these values!
+        self.camera_sensor = self.world.spawn_actor(self.camera, self.camera_spawn_point, attach_to=self.vehicle)
+        self.actor_list.append(self.camera_sensor)
+        self.camera_sensor.listen(lambda data: self.process_sensory_data(data))
 
-    # sensory data
-    camera_bp = blueprint_library.find("sensor.camera.rgb")
-    camera_bp.set_attribute("image_size_x", f"{IM_WIDTH}")
-    camera_bp.set_attribute("image_size_y", f"{IM_HEIGHT}")
-    camera_bp.set_attribute("fov", "110")
+        self.vehicle.apply_control(carla.VehicleControl(throttle=0.0, steer=0.0))
+        time.sleep(3)
 
-    camera_spawn_point = carla.Transform(carla.Location(x=5, z=2))  # TODO: fine-tune these values!
-    sensor = world.spawn_actor(camera_bp, camera_spawn_point, attach_to=vehicle)
-    actor_list.append(sensor)
-    sensor.listen(lambda data: process_sensory_data(data))
-
-    time.sleep(7)
-
-finally:
-    for actor in actor_list:
-        actor.destroy()
-    print("All cleaned up!")
+    def process_sensory_data(self, data):
+        data_arr = np.array(data.raw_data)
+        data_pic = data_arr.reshape((IM_HEIGHT, IM_WIDTH, 4))[:, :, :3]  # we only want rgb!
+        cv2.imshow("", data_pic)
+        cv2.waitKey(1)
+        return data_pic / 255.0  # normalizing sensory data for the neural network
